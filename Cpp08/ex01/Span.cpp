980ce53f73840992e0cf05e3b9571da9f22dc264
omvr-49 <you@example.com>
2025-04-11 02:48:13 +0100

    Cpp08 - ex01

--- Cpp08/ex01/Span.cpp
+++ Cpp08/ex01/Span.cpp
@@ -1,1 +1,32 @@
 #include "Span.hpp"
+
+const char *outOfRange::what() const throw ()
+{
+    return ("Out of Range");
+}
+
+// OCCF:
+Span::Span()
+{
+
+}
+
+Span& Span::operator=(const Span& rhs)
+{
+    if (this != &rhs)
+    {
+        this->N = rhs.N;
+        this->vectorian = rhs.vectorian;
+    }
+    return (*this);
+}
+
+Span::Span(const Span& copy)
+{
+    *this = copy;
+}
+Span::~Span()
+{
+
+}
+ // Ex Requirement:
--- Cpp08/ex01/Span.hpp
+++ Cpp08/ex01/Span.hpp
@@ -1,13 +1,34 @@
+
 #ifndef SPAN_HPP
 #define SPAN_HPP
 
+#include <exception>
 #include <iostream>
 #include <string>
+#include <vector>
 #include <set>
 
-
-
-
-
+class Span
+{
+    private:
+        unsigned int N;
+        std::vector<int> vectorian;
+    public:
+    // OCCF:
+        Span();
+        Span& operator=(const Span& rhs);
+        Span(const Span& other);
+        ~Span();
+    // Ex Requirement:
+        Span(unsigned int MaxN);
+        void addNumber(int number);
+        unsigned int shortestSpan();
+        unsigned int longestSpan();
+        class outOfRange : public std::exception
+        {
+            public:
+                const char* what() const throw();
+        };
+};
 
 #endif
